a235bfe76f7e266b3f7a3f5a4d0ad942079eb4e8
MoJunho3467 <junho3468@naver.com>
2026-09-14 01:53:41 +0900

    fix: clean up backend controllers

--- backend/src/main/java/com/fieldwork/controller/FileController.java
+++ backend/src/main/java/com/fieldwork/controller/FileController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/files")
